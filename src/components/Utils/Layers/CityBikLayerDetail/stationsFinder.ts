e19b5379c2e3e7b6abdaff63769f1c1ea0463690
p3javier <65142395+p3javier@users.noreply.github.com>
2020-12-28 21:33:21 +0100

    ssl added to stationsFinder.ts

--- src/components/Utils/Layers/CityBikLayerDetail/stationsFinder.ts
+++ src/components/Utils/Layers/CityBikLayerDetail/stationsFinder.ts
@@ -1,5 +1,5 @@
 export async function stations(id: string) {
-  const url = `http://api.citybik.es/v2/networks/${id}`;
+  const url = `https://api.citybik.es/v2/networks/${id}`;
   const response = await fetch(url);
   const data = await response.json();
   return data.network.stations;

3c40a830b2810169842326adc7144be7441502f5
p3javier <65142395+p3javier@users.noreply.github.com>
2021-01-11 16:47:04 +0100

    basic test added

---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  testEnviroment: "jest-enviroment-jsdom",
+};
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { Provider } from "react-redux";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import store from "./redux/store";
+
+test("it works", () => {});
+
+test("renders", () => {
+  const { debug } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  debug();
 });

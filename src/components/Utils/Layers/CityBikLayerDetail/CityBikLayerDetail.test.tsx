a6359eb028e8c35df99d0aea37a101018cd62ccb
p3javier <65142395+p3javier@users.noreply.github.com>
2021-01-22 23:46:00 +0100

    New unit tests for CityBikLayerDetail

--- src/components/Utils/Layers/CityBikLayerDetail/CityBikLayerDetail.test.tsx
+++ src/components/Utils/Layers/CityBikLayerDetail/CityBikLayerDetail.test.tsx
@@ -1,17 +1,48 @@
-//@ts-nocheck
-import React from "react";
-import { render } from "@testing-library/react";
-import CityBikLayerDetail from "./CityBikLayerDetail";
+import { networksFinder } from "./networkFinder";
 
-import { MapConsumer } from "react-leaflet";
+import { stations } from "./stationsFinder";
+
+const sampleCoords = [51.109474, 17.033918];
+
+const sampleResponse = [
+  {
+    company: ["Nextbike GmbH"],
+    href: "/v2/networks/wrm-wroclaw",
+    id: "wrm-wroclaw",
+    location: {
+      city: "Wrocław",
+      country: "PL",
+      latitude: 51.1097,
+      longitude: 17.0485,
+    },
+    name: "WRM",
+  },
+  {
+    company: ["Nextbike GmbH"],
+    href: "/v2/networks/wrm-orlen-wroclaw",
+    id: "wrm-orlen-wroclaw",
+    location: {
+      city: "Wrocław",
+      country: "PL",
+      latitude: 51.1097,
+      longitude: 17.0485,
+    },
+    name: "WRM - Orlen",
+  },
+];
 
 describe("CityBikeLayer tests", () => {
-  it("should render", () => {
-    const { debug } = render(
-      <MapConsumer>
-        <CityBikLayerDetail />
-      </MapConsumer>
+  it("networkFinder function is giving the correct response", async () => {
+    const networks = await networksFinder(sampleCoords, [0.1, 0.2]).then(
+      (areaNetworks) => areaNetworks
     );
-    console.log(debug);
+    expect(networks).toEqual(sampleResponse);
+  });
+  it("stationsFinder function is returning the correct stations", async () => {
+    const sampleNetworkId = "wrm-wroclaw";
+    const stationsResponse = await stations(sampleNetworkId).then(
+      (returnedStations) => returnedStations
+    );
+    console.log("STATIONS", stationsResponse);
   });
 });
